6c9ccc5d01a09489894464d8173e4f12b831569c
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-26 05:51:15 +0100

    removed timestamp

--- plugin/database.py
+++ plugin/database.py
@@ -141,7 +141,7 @@ class Database:
         for id_members, name_members in data_members.items():
             try:
                 self.cur.execute('INSERT INTO botzilla.users (ID, name, date_added) VALUES ({}, \'{}\', {});'.format(
-                    id_members, str(name_members), 'current_timestamp'))
+                    id_members, str(name_members), 'NULL'))
             except Exception as e:
                 print('Error gathering info user:\n{}'.format(e.args))
                 continue
