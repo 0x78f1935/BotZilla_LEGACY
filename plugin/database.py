1b3f3b7661c934f70e8c39ccbae81a32d9ce24e6
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-26 04:51:35 +0100

    make string from username

--- plugin/database.py
+++ plugin/database.py
@@ -130,7 +130,7 @@ class Database:
         self.cur.execute('ROLLBACK;')
         for id_members, name_members in data_members.items():
             try:
-                self.cur.execute('INSERT INTO botzilla.users (ID, name) VALUES ({}, "{}");'.format(id_members, name_members))
+                self.cur.execute('INSERT INTO botzilla.users (ID, name) VALUES ({}, "{}");'.format(id_members, str(name_members)))
             except Exception as e:
                 print('While getting user info, Error : {}'.format(e.args))
                 continue
