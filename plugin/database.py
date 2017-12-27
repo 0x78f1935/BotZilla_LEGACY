e494ecde5f08da68f2330e06a0e2313b654fcaa0
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-27 23:01:55 +0100

    fix for endles loop

--- plugin/database.py
+++ plugin/database.py
@@ -381,11 +381,7 @@ class Database:
                     try:
                         self.cur.execute("INSERT INTO botzilla.musicque(url) VALUES('{}');".format(row))
                     except Exception as e:
-                        embed = discord.Embed(title='{}:'.format('Warning'),
-                                              description='```Python\n{}\n```'.format(e.args),
-                                              colour=0xf20006)
-                        a = await self.bot.say(embed=embed)
-                        await self.bot.add_reaction(a, self.emojiUnicode['warning'])
+                        pass
 
                 embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                       description='Done!',
