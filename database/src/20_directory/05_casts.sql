5fc1f36f196fd99dcdf86cd1ecca0f09fc99a5eb
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-27 18:28:36 +0100

    Changes in db

--- database/src/20_directory/05_casts.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION directory.to_char(directory.pointset)
-    RETURNS text
-AS $$
-    SELECT 'point.' || $1."name"
-$$ LANGUAGE sql STABLE STRICT;
-
-CREATE CAST (directory.pointset AS text)
-  WITH FUNCTION directory.to_char(directory.pointset);
