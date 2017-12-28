a70c383bd2bb35a4479f9be574f087db37efef36
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-28 23:39:07 +0100

    youtube support

--- core.py
+++ core.py
@@ -135,7 +135,8 @@ async def on_ready():
                                 rows = str(rows).replace('[(\'', '')
                                 rows = str(rows).replace('\',)]', '')
                                 print(rows)
-                                voice.create_ytdl_player(f"'{rows}'")
+                                player = await voice.create_ytdl_player(f"'{rows}'")
+                                player.start()
                     except Exception as e:
                         print(f'Database seems offline:\n{e.args}')
 
