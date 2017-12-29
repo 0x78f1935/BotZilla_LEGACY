163ea6894c3dfaa6cd0adbfa968cb31307d49f76
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-29 15:09:32 +0100

    change id

--- plugin/music.py
+++ plugin/music.py
@@ -69,16 +69,16 @@ class Music:
         self.voice_states = {}
 
     def get_voice_state(self, server):
-        state = self.voice_states.get(server.id)
+        state = self.voice_states.get(server)
         if state is None:
             state = VoiceState(self.bot)
-            self.voice_states[server.id] = state
+            self.voice_states[server] = state
 
         return state
 
     async def create_voice_client(self, channel: str):
         voice = await self.bot.join_voice_channel(channel)
-        state = self.get_voice_state(channel.server)
+        state = self.get_voice_state(channel.server.id)
         state.voice = voice
 
     def __unload(self):
