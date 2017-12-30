5da6a5a4cdfae68ac4eb6938b1687f6d2fe18f2d
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-30 01:33:19 +0100

    fix in r6s

--- plugin/gamestats.py
+++ plugin/gamestats.py
@@ -17,16 +17,23 @@ class Leagues:
 
 
     @commands.command(pass_context=True)
-    async def r6s(self, ctx, *, uplay_name):
+    async def r6s(self, ctx, *, uplay_name=None):
         """Shows your Rainbow Six Siege stats.
         Use your Uplay username for this command"""
 
-        # https://api.r6stats.com/api/v1/players/username/seasons?platform=PlatForm
-        url = 'https://api.r6stats.com/api/v1/players/{}?platform=uplay'.format(uplay_name)
-        with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as response:
-            source = response.read()
-        data = json.loads(source)
-        template = """
+        if uplay_name is None:
+            embed = discord.Embed(title="{}".format("Bitcoin :currency_exchange:"),
+                                  description="I hope you play the tutorial if you play a new game..\nTry ```{}help r6s``` instead".format(self.config['prefix']),
+                                  color=0xf20006)
+            last_message = await self.bot.say(embed=embed)
+            await self.bot.add_reaction(last_message, self.emojiUnicode['warning'])
+            return
+        else:
+            url = 'https://api.r6stats.com/api/v1/players/{}?platform=uplay'.format(uplay_name)
+            with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as response:
+                source = response.read()
+            data = json.loads(source)
+            template = """
 Player : [PLAYER]
 Platform : [PLATFORM]
 Level : [LVL]
@@ -69,43 +76,43 @@ Stats >>
         Penetration Kills : [PENKILL]
         Assists : [ASSIST]
         """
-        template = template.replace("[PLAYER]", str(data['player']['username']))
-        template = template.replace("[PLATFORM]", str(data['player']['platform']))
-        template = template.replace("[LVL]", str(data['player']['stats']['progression']['level']))
-        template = template.replace("[XP]", str(data['player']['stats']['progression']['xp']))
-        template = template.replace("[DATE]", str(data['player']['updated_at']))
-        template = template.replace("[RANKDEEDMEE]", str(data['player']['stats']['ranked']['has_played']))
-        template = template.replace("[RANKWIN]", str(data['player']['stats']['ranked']['wins']))
-        template = template.replace("[RANKLOS]", str(data['player']['stats']['ranked']['losses']))
-        template = template.replace("[RANKWLR]", str(data['player']['stats']['ranked']['wlr']))
-        template = template.replace("[RANKKIL]", str(data['player']['stats']['ranked']['kills']))
-        template = template.replace("[RANKDEAD]", str(data['player']['stats']['ranked']['deaths']))
-        template = template.replace("[RANKKD]", str(data['player']['stats']['ranked']['kd']))
-        template = template.replace("[RANKPLAY]", str(data['player']['stats']['ranked']['playtime']))
-        template = template.replace("[CASDEEDMEE]", str(data['player']['stats']['casual']['has_played']))
-        template = template.replace("[CASWIN]", str(data['player']['stats']['casual']['wins']))
-        template = template.replace("[CASLOS]", str(data['player']['stats']['casual']['losses']))
-        template = template.replace("[CASWL]", str(data['player']['stats']['casual']['wlr']))
-        template = template.replace("[CASKILL]", str(data['player']['stats']['casual']['kills']))
-        template = template.replace("[CASDEAD]", str(data['player']['stats']['casual']['deaths']))
-        template = template.replace("[CASKD]", str(data['player']['stats']['casual']['kd']))
-        template = template.replace("[CASPLAY]", str(data['player']['stats']['casual']['playtime']))
-        template = template.replace("[REV]", str(data['player']['stats']['overall']['revives']))
-        template = template.replace("[SUC]", str(data['player']['stats']['overall']['suicides']))
-        template = template.replace("[RD]", str(data['player']['stats']['overall']['reinforcements_deployed']))
-        template = template.replace("[BB]", str(data['player']['stats']['overall']['barricades_built']))
-        template = template.replace("[SM]", str(data['player']['stats']['overall']['steps_moved']))
-        template = template.replace("[BF]", str(data['player']['stats']['overall']['bullets_fired']))
-        template = template.replace("[BH]", str(data['player']['stats']['overall']['bullets_hit']))
-        template = template.replace("[H]", str(data['player']['stats']['overall']['headshots']))
-        template = template.replace("[MELEE]", str(data['player']['stats']['overall']['melee_kills']))
-        template = template.replace("[PENKILL]", str(data['player']['stats']['overall']['penetration_kills']))
-        template = template.replace("[ASSIST]", str(data['player']['stats']['overall']['assists']))
-        embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
-                              description=template,
-                              colour=0xf20006)
-        last_message = await self.bot.say(embed=embed)
-        await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
+            template = template.replace("[PLAYER]", str(data['player']['username']))
+            template = template.replace("[PLATFORM]", str(data['player']['platform']))
+            template = template.replace("[LVL]", str(data['player']['stats']['progression']['level']))
+            template = template.replace("[XP]", str(data['player']['stats']['progression']['xp']))
+            template = template.replace("[DATE]", str(data['player']['updated_at']))
+            template = template.replace("[RANKDEEDMEE]", str(data['player']['stats']['ranked']['has_played']))
+            template = template.replace("[RANKWIN]", str(data['player']['stats']['ranked']['wins']))
+            template = template.replace("[RANKLOS]", str(data['player']['stats']['ranked']['losses']))
+            template = template.replace("[RANKWLR]", str(data['player']['stats']['ranked']['wlr']))
+            template = template.replace("[RANKKIL]", str(data['player']['stats']['ranked']['kills']))
+            template = template.replace("[RANKDEAD]", str(data['player']['stats']['ranked']['deaths']))
+            template = template.replace("[RANKKD]", str(data['player']['stats']['ranked']['kd']))
+            template = template.replace("[RANKPLAY]", str(data['player']['stats']['ranked']['playtime']))
+            template = template.replace("[CASDEEDMEE]", str(data['player']['stats']['casual']['has_played']))
+            template = template.replace("[CASWIN]", str(data['player']['stats']['casual']['wins']))
+            template = template.replace("[CASLOS]", str(data['player']['stats']['casual']['losses']))
+            template = template.replace("[CASWL]", str(data['player']['stats']['casual']['wlr']))
+            template = template.replace("[CASKILL]", str(data['player']['stats']['casual']['kills']))
+            template = template.replace("[CASDEAD]", str(data['player']['stats']['casual']['deaths']))
+            template = template.replace("[CASKD]", str(data['player']['stats']['casual']['kd']))
+            template = template.replace("[CASPLAY]", str(data['player']['stats']['casual']['playtime']))
+            template = template.replace("[REV]", str(data['player']['stats']['overall']['revives']))
+            template = template.replace("[SUC]", str(data['player']['stats']['overall']['suicides']))
+            template = template.replace("[RD]", str(data['player']['stats']['overall']['reinforcements_deployed']))
+            template = template.replace("[BB]", str(data['player']['stats']['overall']['barricades_built']))
+            template = template.replace("[SM]", str(data['player']['stats']['overall']['steps_moved']))
+            template = template.replace("[BF]", str(data['player']['stats']['overall']['bullets_fired']))
+            template = template.replace("[BH]", str(data['player']['stats']['overall']['bullets_hit']))
+            template = template.replace("[H]", str(data['player']['stats']['overall']['headshots']))
+            template = template.replace("[MELEE]", str(data['player']['stats']['overall']['melee_kills']))
+            template = template.replace("[PENKILL]", str(data['player']['stats']['overall']['penetration_kills']))
+            template = template.replace("[ASSIST]", str(data['player']['stats']['overall']['assists']))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description=template,
+                                  colour=0xf20006)
+            last_message = await self.bot.say(embed=embed)
+            await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
 
 def setup(bot):
     bot.add_cog(Leagues(bot))
