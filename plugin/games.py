c87c7f19cd4e2461c611f5ead2681f40b3cc6795
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-30 21:58:58 +0100

    Update help

--- plugin/games.py
+++ plugin/games.py
@@ -219,6 +219,11 @@ class Games:
 
     @commands.command(pass_context=True)
     async def joke(self, ctx):
+        """
+        Ever heard a Chuck Norris joke?
+        :param ctx:
+        :return:
+        """
         url = 'http://api.icndb.com/jokes/random%22'
         with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as response:
             source = response.read().decode('utf-8')
