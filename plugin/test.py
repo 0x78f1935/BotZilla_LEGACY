f22de3d04c1dfc994678b0d5aa2e649aca9f5910
Annihilator708 <Annihilator708@users.noreply.github.com>
2017-12-31 19:59:29 +0100

    .

--- plugin/test.py
+++ plugin/test.py
@@ -67,7 +67,17 @@ class TestScripts:
             username = username.replace('<@', '')
             username = username.replace('>', '')
             username = username.replace('!', '')
-            name = await self.bot.get_user_info(username)
+
+            try:
+                name = await self.bot.get_user_info(username)
+            except:
+                embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                      description='Invalid username'.format(str(username)),
+                                      colour=0xf20006)
+                a = await self.bot.say(embed=embed)
+                await self.bot.add_reaction(a, self.emojiUnicode['warning'])
+                return
+
             embed = discord.Embed(title='Blacklist vote started by {}:'.format(ctx.message.author.name),
                                   description='Total votes are needed: **{}**\nWould you like to blacklist:\n\n**`{}`**\n\nReason:\n\n**`{}`**\n\nPeople who got blacklisted can\'t use BotZilla anymore.\nEven in other servers'.format(
                                       vote_policy, name, str(reason)),
@@ -76,9 +86,9 @@ class TestScripts:
             await self.bot.add_reaction(a, '\u2705')
             await self.bot.add_reaction(a, '\U0001f1fd')
             await asyncio.sleep(10)
+
             message = await self.bot.get_message(ctx.message.channel, a.id)
             total = message.reactions[0].count - 1
-            #
 
             if total >= vote_policy:
                 print(f'Vote approved for {username}')
@@ -88,12 +98,7 @@ class TestScripts:
                                       colour=0xf20006)
                 a = await self.bot.say(embed=embed)
                 await self.bot.add_reaction(a, '\u2705')
-            # except:
-            #     embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
-            #                           description='Invalid username'.format(str(username)),
-            #                           colour=0xf20006)
-            #     a = await self.bot.say(embed=embed)
-            #     await self.bot.add_reaction(a, self.emojiUnicode['warning'])
+
 
 def setup(bot):
     bot.add_cog(TestScripts(bot))
