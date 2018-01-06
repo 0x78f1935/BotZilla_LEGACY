412027cf29c7e095a5bfaaa44af7c293a75e8eb1
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-01-06 22:28:24 +0100

    fix bug?

--- plugin/exchange.py
+++ plugin/exchange.py
@@ -25,7 +25,7 @@ class MoneyMaker:
         url = tmp_config['exchange']['api-url']
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
-                source = await response.json(encoding='utf8')
+                source = await response.json()
 
         source = json.dumps(source)
         data = json.loads(str(source))
