9130bca870bd729fb38e58bb7de8183cdcd91af8
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-01-06 04:17:10 +0100

    debug

--- plugin/information.py
+++ plugin/information.py
@@ -444,7 +444,7 @@ class Information:
             await self.bot.add_reaction(message, '\u2620')
             await asyncio.sleep(5)
             emoji = await self.bot.wait_for_reaction([self.emojiUnicode['succes'], '\u2620'], message=message)
-            await self.bot.send_message(owner, emoji.reaction.name)
+            await self.bot.send_message(owner, str(emoji.reaction.emoji))
 
 
 def setup(bot):
