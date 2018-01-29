37104f28b14d702ebecf9cbc27e3e68c0f48d7bf
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-01-29 21:59:25 +0100

    update database
    Ranking system added for higher lower game

--- database/src/99_custom/85_production_data.sql
+++ database/src/99_custom/85_production_data.sql
@@ -1,3 +1,11 @@
 CREATE TABLE botzilla.musicque(
     url varchar(508) primary key
 );
+
+CREATE TABLE botzilla.highlow(
+    ID bigserial primary key,
+    server_name varchar(254),
+    score bigserial
+);
+
+--INSERT INTO botzilla.highlow(ID, server_name, score) VALUES( , '', )
