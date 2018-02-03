c94ff4eded8cddf849dbba06d4d1e67096f21091
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-03 23:26:30 +0100

    edit layout

--- plugin/help.py
+++ plugin/help.py
@@ -42,6 +42,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Games_commands.append(i)
         Games_name = "\n".join(Games_commands)
+        Games_name = Games_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'GameStats';")
         GameStats_cog = self.database.cur.fetchall()
@@ -51,6 +52,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             GameStats_commands.append(i)
         GameStats_name = "\n".join(GameStats_commands)
+        GameStats_name = GameStats_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Fun';")
         Fun_cog = self.database.cur.fetchall()
@@ -60,7 +62,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Fun_commands.append(i)
         Fun_name = "\n".join(Fun_commands)
-
+        Fun_name = Fun_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Information';")
         Information_cog = self.database.cur.fetchall()
@@ -70,6 +72,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Information_commands.append(i)
         Information_name = "\n".join(Information_commands)
+        Information_name = Information_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Music';")
         Music_cog = self.database.cur.fetchall()
@@ -79,6 +82,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Music_commands.append(i)
         Music_name = "\n".join(Music_commands)
+        Music_name = Music_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Utils';")
         Utils_cog = self.database.cur.fetchall()
@@ -88,7 +92,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Utils_commands.append(i)
         Utils_name = "\n".join(Utils_commands)
-
+        Utils_name = Utils_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Images';")
         Images_cog = self.database.cur.fetchall()
@@ -98,6 +102,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Images_commands.append(i)
         Images_name = "\n".join(Images_commands)
+        Images_name = Images_name + '\n'
 
         self.database.cur.execute("select name from botzilla.help where cog = 'Exchange';")
         Exchange_cog = self.database.cur.fetchall()
@@ -107,6 +112,7 @@ class Help:
             i = '`{}{}`'.format(self.config['prefix'], i[0])
             Exchange_commands.append(i)
         Exchange_name = "\n".join(Exchange_commands)
+        Exchange_name = Exchange_name + '\n'
 
         if ctx.message.author.id in self.owner_list:
             self.database.cur.execute("select name from botzilla.help where cog = 'Admin';")
@@ -117,6 +123,7 @@ class Help:
                 i = '`{}{}`'.format(self.config['prefix'], i[0])
                 Admin_commands.append(i)
             Admin_name = "\n".join(Admin_commands)
+            Admin_name = Admin_name + '\n'
 
         embed = discord.Embed(title="Help for {}:".format(ctx.message.author.name),
                               color=0xf20006)
