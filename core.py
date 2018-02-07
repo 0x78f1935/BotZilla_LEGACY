d3ad1d5154f928ae9f8e55fc0be3b5b18003fdc0
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-07 17:57:41 +0100

    game status change 10 > 8 seconds

--- core.py
+++ core.py
@@ -153,10 +153,10 @@ async def total_online_user_tracker():
     while True:
         game = discord.Game(name='{} online users'.format(sum(1 for m in set(bot.get_all_members()) if m.status != discord.Status.offline)), type=3)
         await bot.change_presence(game=game)
-        await asyncio.sleep(10)
+        await asyncio.sleep(8)
         game = discord.Game(name='{}help'.format(config['prefix']), type=2)
         await bot.change_presence(game=game)
-        await asyncio.sleep(10)
+        await asyncio.sleep(8)
 
 
 @bot.event
