2e5c4ee0de510c7fb388130043b5ed9f02835b4f
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-18 18:00:51 +0100

    send typing

--- plugin/music.py
+++ plugin/music.py
@@ -279,6 +279,7 @@ class Music:
         The list of supported sites can be found here:
         https://rg3.github.io/youtube-dl/supportedsites.html
         """
+        await self.bot.send_typing()
         state = self.get_voice_state(ctx.message.server)
         ytdl = youtube_dl.YoutubeDL({
             "format": 'best',
