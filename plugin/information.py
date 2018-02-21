c1633516f4a22305614ec8f4bf68702df6e9199b
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-21 20:44:48 +0100

    fix

--- plugin/information.py
+++ plugin/information.py
@@ -682,21 +682,22 @@ class Utils:
                                       description=f"Poll started by : **`{ctx.message.author.name}`**\nID number : **`{ctx.message.author.id}`**\nQuestion was :\n**```\n{question}\n```**",
                                       colour=0xf20006)
 
-                message = await self.bot.say(embed=embed)
-                await self.bot.add_reaction(message, self.emojiUnicode['succes'])
+                b = await self.bot.say(embed=embed)
+                await self.bot.add_reaction(b, self.emojiUnicode['succes'])
 
                 # check for loop #round
                 round = 0
                 for i in range(10-1):
-                    message = await self.bot.get_message(ctx.message.channel, message.id)
+                    b = await self.bot.get_message(ctx.message.channel, b.id)
                     embed = discord.Embed(title='Results of poll:',
                                           description='\t',
                                           colour=0xf20006)
                     for key, value in answerpoll.items():
                         embed.add_field(name='{}'.format(str(key).upper()), value='{}'.format(str(value)), inline=False)
-                    print(f'answers added {key} {value}')
+                        print(f'answers added {key} {value}')
                     embed.add_field(name='The server has chosen answer :', value='**`{}`**'.format(str(answers_user[winner]).upper()))
-                    message = await self.bot.edit_message(message, embed=embed)
+                    print(embed)
+                    b = await self.bot.edit_message(b, embed=embed)
                     await self.bot.add_reaction(message, self.emojiUnicode['succes'])
                     # so the loop ends at the right position
                     round += 1
@@ -705,17 +706,12 @@ class Utils:
 
                     await asyncio.sleep(5)
 
-                    message = await self.bot.get_message(ctx.message.channel, message.id)
+                    b = await self.bot.get_message(ctx.message.channel, b.id)
                     embed = discord.Embed(title='Results of poll:',
                                           description=f"Poll started by : **`{ctx.message.author.name}`**\nID number : **`{ctx.message.author.id}`**\nQuestion was :\n**```\n{question}\n```**",
                                           colour=0xf20006)
-                    for key, value in answerpoll.items():
-                        embed.add_field(name='\t', value='{}: {}\n'.format(key, value), inline=False)
-                    print(f'answers added {key} {value}')
-                    embed.add_field(name='The server has chosen answer :', value='**`{}`**'.format(str(answers_user[winner]).upper()))
-                    print(embed, type(embed), str(embed))
-                    message = await self.bot.edit_message(message, embed=embed)
-                    await self.bot.add_reaction(message, self.emojiUnicode['succes'])
+                    b = await self.bot.edit_message(b, embed=embed)
+                    await self.bot.add_reaction(b, self.emojiUnicode['succes'])
 
             except Exception as e:
                 embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
