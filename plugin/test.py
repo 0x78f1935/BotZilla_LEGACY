2a75e32d21e60be53ab48b4816620edea5cb58e3
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-25 00:44:44 +0100

    update score

--- plugin/test.py
+++ plugin/test.py
@@ -169,6 +169,11 @@ class TestScripts:
                                       colour=0xf20006)
                 a = await self.bot.say(embed=embed)
                 await self.bot.add_reaction(a, self.emojiUnicode['succes'])
+                board[user_row][user_col] = "2"
+                board_db_insert = str(board).replace("'", "<A>").replace(",", "<C>")  # make seperater for db, A for ' C for ,
+                score += 1
+                self.database.cur.execute(f"UPDATE botzilla.battleship SET board = {board_db_insert}, score = {score} where ID = {id};")
+                self.database.cur.execute("ROLLBACK;")
             else:
                 if user_row not in range(5) or user_col not in range(5):
                     embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
