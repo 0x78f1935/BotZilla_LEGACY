32e0351e2fa9280f2b5dc5039f81a41d7e7bca92
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-24 21:27:54 +0100

    prep battleship

---
+++ options/battleship.js
@@ -0,0 +1,11 @@
+{
+    "one" : ":one:",
+    "two" : ":two:",
+    "three" : ":three:",
+    "four" : ":four:",
+    "five" : ":five:",
+    "ocean" : ":ocean:",
+    "bomb" : ":bomb:",
+    "x" : ":x:",
+    "fire" : ":fire:"
+}
