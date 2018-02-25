e51ca4ef2d62c627355630951190ae5201b8b076
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-25 03:45:54 +0100

    fix bomb

--- database/src/00_base.sql
+++ database/src/00_base.sql
@@ -21,7 +21,7 @@ CREATE TABLE botzilla.music(
 CREATE TABLE botzilla.battleship(
     ID bigserial primary key,
     gamehash varchar(508),
-    board varchar(1024),
+    board varchar(1700),
     score varchar(508),
     ship_row varchar(508),
     ship_col varchar(508)
