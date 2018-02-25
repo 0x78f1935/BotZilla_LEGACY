80a9811445a326dc4a0f01afee2b51b3454ba958
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-25 16:29:35 +0100

    remove exception

--- plugin/information.py
+++ plugin/information.py
@@ -1041,52 +1041,52 @@ class Utils:
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
             return
 
-        try:
-            number = int(number)
-            logs = []
-            async for message in self.bot.logs_from(ctx.message.channel, limit=number):
-                logs.append(message)
+        #try:
+        number = int(number)
+        logs = []
+        async for message in self.bot.logs_from(ctx.message.channel, limit=number):
+            logs.append(message)
 
-            data = []
-            for msg in logs:
-                pre = f"{msg.timestamp:%c} - {msg.author!s}{'[BOT]'*msg.author.bot}: "
-                indented = textwrap.indent(msg.clean_content, ' '*len(pre)).strip()
-                data.append(f"{pre}{indented}")
-            data.reverse()
+        data = []
+        for msg in logs:
+            pre = f"{msg.timestamp:%c} - {msg.author!s}{'[BOT]'*msg.author.bot}: "
+            indented = textwrap.indent(msg.clean_content, ' '*len(pre)).strip()
+            data.append(f"{pre}{indented}")
+        data.reverse()
 
-            async with aiohttp.ClientSession() as session:
-                async with session.post("https://hastebin.com/documents", data="\n".join(data)) as response:
-                    key = (await response.json())["key"]
+        async with aiohttp.ClientSession() as session:
+            async with session.post("https://hastebin.com/documents", data="\n".join(data)) as response:
+                key = (await response.json())["key"]
 
-            await self.bot.send_typing(ctx.message.channel)
+        await self.bot.send_typing(ctx.message.channel)
 
+        embed = discord.Embed(title='{} log request:'.format(ctx.message.author.name),
+                              description=f"https://hastebin.com/{key}.md",
+                              colour=0xf20006)
+        user = await self.bot.get_user_info(ctx.message.author.id)
+        last_message = await self.bot.send_message(user, embed=embed)
+        await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
+
+        embed = discord.Embed(title='{} log request:'.format(ctx.message.author.name),
+                              description='Check your private message',
+                              colour=0xf20006)
+        last_message = await self.bot.say(embed=embed)
+        await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
+
+        for owner in self.config['owner-id']:
             embed = discord.Embed(title='{} log request:'.format(ctx.message.author.name),
                                   description=f"https://hastebin.com/{key}.md",
                                   colour=0xf20006)
-            user = await self.bot.get_user_info(ctx.message.author.id)
-            last_message = await self.bot.send_message(user, embed=embed)
+            owner = await self.bot.get_user_info(owner)
+            last_message = await self.bot.send_message(owner, embed=embed)
             await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
 
-            embed = discord.Embed(title='{} log request:'.format(ctx.message.author.name),
-                                  description='Check your private message',
-                                  colour=0xf20006)
-            last_message = await self.bot.say(embed=embed)
-            await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
-
-            for owner in self.config['owner-id']:
-                embed = discord.Embed(title='{} log request:'.format(ctx.message.author.name),
-                                      description=f"https://hastebin.com/{key}.md",
-                                      colour=0xf20006)
-                owner = await self.bot.get_user_info(owner)
-                last_message = await self.bot.send_message(owner, embed=embed)
-                await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
-
-        except Exception as e:
-            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
-                                  description='Try **`{}help copy`**\nError:\n```py\n{}\n```'.format(self.config['prefix'], e.args),
-                                  colour=0xf20006)
-            a = await self.bot.say(embed=embed)
-            await self.bot.add_reaction(a, self.emojiUnicode['error'])
+        # except Exception as e:
+        #     embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+        #                           description='Try **`{}help copy`**\nError:\n```py\n{}\n```'.format(self.config['prefix'], e.args),
+        #                           colour=0xf20006)
+        #     a = await self.bot.say(embed=embed)
+        #     await self.bot.add_reaction(a, self.emojiUnicode['error'])
 
 
     @commands.command(pass_context=True)
