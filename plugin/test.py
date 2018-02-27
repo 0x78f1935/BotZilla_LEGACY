5e51af8012e0f584feec4cc04bea0fdf145af2da
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-02-27 15:06:09 +0100

    .

--- plugin/test.py
+++ plugin/test.py
@@ -423,6 +423,10 @@ class TestScripts:
                                                   f"{header}\n{self.battleship_emoji_text['one']} {row_1}\n{self.battleship_emoji_text['two']} {row_2}\n{self.battleship_emoji_text['three']} {row_3}\n{self.battleship_emoji_text['four']} {row_4}\n{self.battleship_emoji_text['five']} {row_5}\n{self.battleship_emoji_text['six']} {row_6}\n{self.battleship_emoji_text['seven']} {row_7}\n{self.battleship_emoji_text['eight']} {row_8}\n{self.battleship_emoji_text['nine']} {row_9}\n{self.battleship_emoji_text['ten']} {row_10}"
                                                   f"\n\nGameHash:\n**{gamehash_1}\n{gamehash_2}**\nIf you are stuck\nuse **`{self.config['prefix']}help battleship`**",
                                       colour=0xf20006)
+                if online != 'False' and enemy != '0':
+                    enemy_player = await self.bot.get_user_info(int(enemy))
+                    embed.add_field(name='You are fighting against a player!', value=f'**`{enemy_player.name}`**')
+
                 embed.set_footer(text='PuffDip#5369 ©')
                 a = await self.bot.say(embed=embed)
                 await self.bot.add_reaction(a, self.emojiUnicode['succes'])
@@ -451,6 +455,10 @@ class TestScripts:
                                                   f"{header}\n{self.battleship_emoji_text['one']} {row_1}\n{self.battleship_emoji_text['two']} {row_2}\n{self.battleship_emoji_text['three']} {row_3}\n{self.battleship_emoji_text['four']} {row_4}\n{self.battleship_emoji_text['five']} {row_5}\n{self.battleship_emoji_text['six']} {row_6}\n{self.battleship_emoji_text['seven']} {row_7}\n{self.battleship_emoji_text['eight']} {row_8}\n{self.battleship_emoji_text['nine']} {row_9}\n{self.battleship_emoji_text['ten']} {row_10}"
                                                   f"\n\nGameHash:\n**{gamehash_1}\n{gamehash_2}**\nIf you are stuck\nuse **`{self.config['prefix']}help battleship`**",
                                       colour=0xf20006)
+                if online != 'False' and enemy != '0':
+                    enemy_player = await self.bot.get_user_info(int(enemy))
+                    embed.add_field(name='A player ship sank in perfect clear water..', value=f'**`{enemy_player.name}`** lost his ship')
+
                 embed.set_footer(text='PuffDip#5369 ©')
                 embed.set_thumbnail(url=random.choice(self.battleship_emoji_text['exploded_boats']))
                 a = await self.bot.say(embed=embed)
@@ -461,6 +469,8 @@ class TestScripts:
                     board.append(['O'] * 10)
                 board_str = str(board).replace("'", "<A>").replace(",", "<C>")  # make seperater for db, A for ' C for
                 score_int = score + 1
+                if online != 'False' and enemy != '0':
+                    score_int += 2
                 row_str = random.randint(0, len(board) - 1)
                 col_int = random.randint(0, len(board[0]) - 1)
                 online_bool = 'False'
@@ -488,6 +498,9 @@ class TestScripts:
                                                       f"{header}\n{self.battleship_emoji_text['one']} {row_1}\n{self.battleship_emoji_text['two']} {row_2}\n{self.battleship_emoji_text['three']} {row_3}\n{self.battleship_emoji_text['four']} {row_4}\n{self.battleship_emoji_text['five']} {row_5}\n{self.battleship_emoji_text['six']} {row_6}\n{self.battleship_emoji_text['seven']} {row_7}\n{self.battleship_emoji_text['eight']} {row_8}\n{self.battleship_emoji_text['nine']} {row_9}\n{self.battleship_emoji_text['ten']} {row_10}"
                                                       f"\n\nGameHash:\n**{gamehash_1}\n{gamehash_2}**\nIf you are stuck\nuse **`{self.config['prefix']}help battleship`**",
                                           colour=0xf20006)
+                    if online != 'False' and enemy != '0':
+                        enemy_player = await self.bot.get_user_info(int(enemy))
+                        embed.add_field(name='You are fighting against a player!', value=f'**`{enemy_player.name}`**')
                     embed.set_footer(text='PuffDip#5369 ©')
                     embed.set_thumbnail(url=random.choice(self.battleship_emoji_text['unexploded_boats']))
                     a = await self.bot.say(embed=embed)
@@ -515,6 +528,10 @@ class TestScripts:
                                                       f"{header}\n{self.battleship_emoji_text['one']} {row_1}\n{self.battleship_emoji_text['two']} {row_2}\n{self.battleship_emoji_text['three']} {row_3}\n{self.battleship_emoji_text['four']} {row_4}\n{self.battleship_emoji_text['five']} {row_5}\n{self.battleship_emoji_text['six']} {row_6}\n{self.battleship_emoji_text['seven']} {row_7}\n{self.battleship_emoji_text['eight']} {row_8}\n{self.battleship_emoji_text['nine']} {row_9}\n{self.battleship_emoji_text['ten']} {row_10}"
                                                       f"\n\nGameHash:\n**{gamehash_1}\n{gamehash_2}**\nIf you are stuck\nuse **`{self.config['prefix']}help battleship`**",
                                           colour=0xf20006)
+                    if online != 'False' and enemy != '0':
+                        enemy_player = await self.bot.get_user_info(int(enemy))
+                        embed.add_field(name='You are fighting against a player!', value=f'**`{enemy_player.name}`**')
+
                     embed.set_footer(text='PuffDip#5369 ©')
                     embed.set_thumbnail(url=random.choice(self.battleship_emoji_text['unexploded_boats']))
                     a = await self.bot.say(embed=embed)
@@ -543,6 +560,9 @@ class TestScripts:
                                                       f"{header}\n{self.battleship_emoji_text['one']} {row_1}\n{self.battleship_emoji_text['two']} {row_2}\n{self.battleship_emoji_text['three']} {row_3}\n{self.battleship_emoji_text['four']} {row_4}\n{self.battleship_emoji_text['five']} {row_5}\n{self.battleship_emoji_text['six']} {row_6}\n{self.battleship_emoji_text['seven']} {row_7}\n{self.battleship_emoji_text['eight']} {row_8}\n{self.battleship_emoji_text['nine']} {row_9}\n{self.battleship_emoji_text['ten']} {row_10}"
                                                       f"\n\nGameHash:\n**{gamehash_1}\n{gamehash_2}**\nIf you are stuck\nuse **`{self.config['prefix']}help battleship`**",
                                           colour=0xf20006)
+                    if online != 'False' and enemy != '0':
+                        enemy_player = await self.bot.get_user_info(int(enemy))
+                        embed.add_field(name='You are fighting against a player!', value=f'**`{enemy_player.name}`**')
                     embed.set_footer(text='PuffDip#5369 ©')
                     embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/407238426417430539/417154157724827668/miss.jpg')
                     a = await self.bot.say(embed=embed)
