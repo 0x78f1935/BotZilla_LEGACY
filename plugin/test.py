8577b0614906025d7f9d1a771059df570aaaf1e7
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-01 16:15:55 +0100

    fix query

--- plugin/test.py
+++ plugin/test.py
@@ -334,7 +334,7 @@ class TestScripts:
             self.database.cur.execute(f"select name_item from cr.c_steal where city = '{city}';")
             steal = self.database.cur.fetchall()
             self.database.cur.execute("ROLLBACK;")
-            self.database.cur.execute(f"select name_item from cr.c_steal where city = '{city}';")
+            self.database.cur.execute(f"select * from cr.c_city where city = '{city}';")
             city = self.database.cur.fetchone()
             self.database.cur.execute("ROLLBACK;")
             things_to_steal = []
