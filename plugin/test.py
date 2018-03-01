2209249683140b646f8edaebd51c3f810e21fd0c
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-01 13:42:40 +0100

    .

--- plugin/test.py
+++ plugin/test.py
@@ -170,7 +170,7 @@ class TestScripts:
             now = datetime.datetime.now()
             future = datetime.datetime.strptime(jail[1], '%Y-%m-%d %H:%M:%S')
             time_to_wait = future - now
-            time_to_wait = datetime.datetime.strptime(time_to_wait, '%Y-%m-%d %H:%M:%S')
+            time_to_wait = datetime.datetime.strptime(str(time_to_wait), '%Y-%m-%d %H:%M:%S')
             if jt:
                 embed = discord.Embed(title='{}:'.format(item[0][2]),
                                       description=f'You are in **jail**. You are **free** at : **`{jail[1]}`**\nThere for you need to **wait** another : **`{time_to_wait}`**',
