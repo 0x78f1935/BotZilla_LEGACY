190e29150baa1a2b5eba7f38b3120ca2c88a7504
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-21 22:43:02 +0100

    add eth

--- plugin/exchange.py
+++ plugin/exchange.py
@@ -28,7 +28,7 @@ class Exchange:
             print('Test: Database files not found - {}'.format(e.args))
             pass
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=["btc"])
     async def bitcoin(self, ctx):
         """
         Shows current bitcoin value
@@ -45,6 +45,28 @@ class Exchange:
 
         embed = discord.Embed(title="{}".format("Bitcoin :currency_exchange:"),
                               description="Bitcoin price is currently at $**{}**".format(data['bpi']['USD']['rate']),
+                              color=0xf20006)
+        last_message = await self.bot.say(embed=embed)
+        await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
+
+
+    @commands.command(pass_context=True, aliases=["eth"])
+    async def ethereum(self, ctx):
+        """
+        Shows current Ethereum value
+        Show Ethereum valua from exchange
+        """
+        print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!Ethereum in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+        url = 'https://api.coinmarketcap.com/v1/ticker/Ethereum/'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                source = await response.json()
+
+        source = json.dumps(source)
+        data = json.loads(str(source))
+
+        embed = discord.Embed(title="{}".format("Ethereum :currency_exchange:"),
+                              description="Ethereum price is currently at $**{},-**".format(round(float(data[0]['price_usd']))),
                               color=0xf20006)
         last_message = await self.bot.say(embed=embed)
         await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
