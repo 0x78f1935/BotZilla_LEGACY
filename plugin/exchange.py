a15d04abb49be78a6c8172290f7157a510871136
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-21 22:47:14 +0100

    .

--- plugin/exchange.py
+++ plugin/exchange.py
@@ -49,7 +49,6 @@ class Exchange:
         last_message = await self.bot.say(embed=embed)
         await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
 
-
     @commands.command(pass_context=True, aliases=["eth"])
     async def ethereum(self, ctx):
         """
