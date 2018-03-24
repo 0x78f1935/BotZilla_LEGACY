5fc4834d146623d8bbd0fe911db5a4e9416ae499
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-24 12:39:45 +0100

    fix bug

--- plugin/fun.py
+++ plugin/fun.py
@@ -294,7 +294,6 @@ class Images:
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!avatar <{member.name}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
         user = member or ctx.message.author
-        user = await self.bot.get_user_info(user.id)
 
         embed = discord.Embed(title='{}\'s avatar:'.format(user.name),
                               description='\t',
