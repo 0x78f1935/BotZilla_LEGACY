3d348ec3e7fb28f8a3ee7795cc2156f263d94a02
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-24 17:27:53 +0100

    fix bug

--- plugin/admin.py
+++ plugin/admin.py
@@ -519,7 +519,7 @@ class AdminCommands:
         im = random.randint(0, int(total_sebi) - 1)
 
         embed = discord.Embed(title='\t', description='\t', color=0xf20006)
-        embed.set_image(url=source[im])
+        embed.set_image(url=source[str(im)])
         embed.set_footer(text="Data © Sebi\'s Bot Tutorial contributors, discord.gg/GWdhBSp")
         await self.bot.say(embed=embed)
 
