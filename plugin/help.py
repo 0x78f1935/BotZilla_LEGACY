8ca86f1f6f3e3a8c2a1962fdb69ef90df03e8b1e
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-25 06:39:13 +0200

    fix typo

--- plugin/help.py
+++ plugin/help.py
@@ -269,7 +269,7 @@ class Help:
         await asyncio.sleep(0.6)
         await self.bot.say('Ready...')
 
-        reaction = await self.bot.wait_for_reaction([self.emoji_start, self.emoji_five_back, self.emoji_oneback, self.emoji_oneahead, self.emoji_five_ahead, self.emoji_end], message=page)
+        reaction = await self.bot.wait_for_reaction([self.emoji_start, self.emoji_five_back, self.emoji_oneback, self.emoji_oneahead, self.emoji_five_ahead, self.emoji_end], message=start)
         if reaction.user.id == ctx.message.author.id:
             await self.bot.edit_message(start, embed=page1)
 
