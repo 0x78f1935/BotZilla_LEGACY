8318222d09754594d88bd630f3e4ba0857f05d60
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-25 15:58:06 +0200

    test

--- plugin/help.py
+++ plugin/help.py
@@ -268,7 +268,8 @@ class Help:
                                      colour=0xf20006)
             split = lambda x, n: x if not x else [x[:n]] + [split([] if not -(len(x) - n) else x[-(len(x) - n):], n)][0]
             page_number = 0
-            for item in split(data, 2)[page_number]:
+            new_list = split(data, 2)[page_number]
+            for item in new_list:
                 new_page.add_field(name=f"Category: **`{cog}`**\n{self.config['prefix']}{item[0]}\n\n",
                                 value=get_short_desc(item),
                                 inline=False)
