82d8f9d0baabdc6a9f62b52804f7fa180c563cf9
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-26 00:27:10 +0200

    add page_number at bottem of help

--- plugin/help.py
+++ plugin/help.py
@@ -385,7 +385,7 @@ class Help:
                     print(page_number)
 
             embed = paginator[str(page_number)]
-            embed.set_footer(text=f'**`Page:`** `{int(page_number + 1)}` / `{int(len(paginator.keys()))}`')
+            embed.set_footer(text=f'Page: [{int(page_number + 1)}]//[{int(len(paginator.keys()))}]')
             await self.bot.edit_message(start, embed=embed)
 
 def setup(bot):
