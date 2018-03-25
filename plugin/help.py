ac487b88ee7a535dc3d923ec8be4131ed0049699
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-25 14:11:18 +0200

    test

--- plugin/help.py
+++ plugin/help.py
@@ -253,7 +253,7 @@ class Help:
         async def wait_for_reaction(message, new_page):
             reaction = await self.bot.wait_for_reaction([self.emoji_start, self.emoji_five_back, self.emoji_oneback, self.emoji_oneahead, self.emoji_five_ahead, self.emoji_end], message=message)
             if ctx.message.author.id == reaction.user.id:
-                return await self.bot.edit_message(message, embed=new_page)
+                return await self.bot.edit_message(message, embed=new_page), reaction
             else:
                 await wait_for_reaction(message, new_page)
 
@@ -299,10 +299,10 @@ class Help:
         await asyncio.sleep(0.6)
         await self.bot.say('Ready...')
 
-        new_page = await wait_for_reaction(start, page1)
-        await self.bot.say(new_page.user.id)
-        new_page = await wait_for_reaction(new_page, page2)
-        await self.bot.say(new_page.user.id)
+        new_page, reaction = await wait_for_reaction(start, page1)
+        await self.bot.say(reaction.user.id)
+        new_page, reaction = await wait_for_reaction(new_page, page2)
+        await self.bot.say(reaction.user.id)
 
 
 
