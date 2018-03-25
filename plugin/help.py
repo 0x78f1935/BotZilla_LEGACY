6d673ccda6f265d1d16a4c51fe51ba6f46ba8b5d
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-25 07:04:02 +0200

    test

--- plugin/help.py
+++ plugin/help.py
@@ -251,7 +251,7 @@ class Help:
         def wait_for_reaction(message):
             reaction = self.bot.wait_for_reaction([self.emoji_start, self.emoji_five_back, self.emoji_oneback, self.emoji_oneahead, self.emoji_five_ahead, self.emoji_end], message=message)
             print(reaction)
-            print(reaction.user)
+
             print(ctx.message.author.id)
             if str(reaction.user.id) != str(ctx.message.author.id):
                 print('not user')
