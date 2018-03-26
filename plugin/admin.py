06a87790866feacfb8deb3569bdb7f0ba9110468
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-26 16:18:55 +0200

    Update help for admin commands

--- plugin/admin.py
+++ plugin/admin.py
@@ -40,15 +40,20 @@ class AdminCommands:
             print('AdminPanel: Database files not found')
             pass
 
-
     @commands.command(pass_context=True, hidden=True)
     async def kick(self, ctx, member:discord.Member = None):
         """
-        Kicks a `Member` from the server they belong to.
-        This function kicks the `Member` based on the server it belongs to,
-        So you must have the proper permissions in that server.
+        Kick a discord member from your server.
+        You must have the proper permissions in the server where you use the kick command.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!kick <user.mention | user.id | user.name>
+        Example:
+          - !!kick PuffDip
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!kick <{member}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+        
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -66,6 +71,13 @@ class AdminCommands:
 
         try:
             await self.bot.kick(member)
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description='You kicked **`{}`** from **`{}`**'.format(
+                                      member.mention, ctx.message.server.name),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
+            await self.bot.add_reaction(a, self.emojiUnicode['succes'])
+
         except Exception as e:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='You may not use this command you do not have permission in server:\n{}'.format(
@@ -73,7 +85,6 @@ class AdminCommands:
                                   colour=0xf20006)
             a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['warning'])
-
 
     # ===========================
     #   Module related commands
@@ -83,8 +94,15 @@ class AdminCommands:
     async def load(self, ctx,  *, extension: str):
         """
         Load an extension.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!load <cogname>
+        Example:
+          - !!load fun
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!load <{extension}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -97,19 +115,31 @@ class AdminCommands:
             self.bot.load_extension("plugin.{}".format(extension))
         except Exception as e:
             traceback.print_exc()
-            a = await self.bot.say("Could not load `{}` -> `{}`".format(extension, e))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Could not load `{}` -> `{}`".format(extension, e),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
         else:
-            a = await self.bot.say("Loaded cog `plugin.{}`.".format(extension))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Loaded cog `plugin.{}`.".format(extension),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['succes'])
-
 
     @commands.command(pass_context=True, hidden=True)
     async def unload(self, ctx, *, extension: str):
         """
         Unload an extension.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!unload <cogname>
+        Example:
+          - !!unload fun
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!unload <{extension}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -122,19 +152,31 @@ class AdminCommands:
             self.bot.unload_extension("plugin.{}".format(extension))
         except Exception as e:
             traceback.print_exc()
-            a = await self.bot.say("Could not unload `{}` -> `{}`".format(extension, e))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Could not unload `{}` -> `{}`".format(extension, e),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
         else:
-            a = await self.bot.say("Unloaded `{}`.".format(extension))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Unloaded `{}`.".format(extension),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['succes'])
-
 
     @commands.command(pass_context=True, hidden=True)
     async def reload(self, ctx, *, extension: str):
         """
         Reload an extension.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!reload <cogname>
+        Example:
+          - !!reload fun
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!reload <{extension}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -148,19 +190,31 @@ class AdminCommands:
             self.bot.load_extension("plugin.{}".format(extension))
         except Exception as e:
             traceback.print_exc()
-            a = await self.bot.say("Could not reload `{}` -> `{}`".format(extension, e))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Could not reload `{}` -> `{}`".format(extension, e),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
         else:
-            a = await self.bot.say("Reloaded `{}`.".format(extension))
+            embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                  description="Reloaded `{}`.".format(extension),
+                                  colour=0xf20006)
+            a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['succes'])
-
 
     @commands.command(pass_context=True, hidden=True)
     async def reloadall(self, ctx):
         """
         Reload all extensions.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!reloadall
+        Example:
+          - !!reloadall
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!reloadall in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -173,19 +227,31 @@ class AdminCommands:
                 self.bot.unload_extension(extension)
                 self.bot.load_extension(extension)
             except Exception as e:
-                a = await self.bot.say("Could not reload `{}` -> `{}`".format(extension, e))
+                embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                                      description="Could not reload `{}` -> `{}`".format(extension, e),
+                                      colour=0xf20006)
+                a = await self.bot.say(embed=embed)
                 await self.bot.add_reaction(a, self.emojiUnicode['error'])
 
-        a = await self.bot.say("Reloaded all.")
+        embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
+                              description="Reloaded all.",
+                              colour=0xf20006)
+        a = await self.bot.say(embed=embed)
         await self.bot.add_reaction(a, self.emojiUnicode['succes'])
-
 
     @commands.command(pass_context=True, hidden=True)
     async def sendalldm(self, ctx, *, content: str = None):
         """
         Mass DM everyone in database
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!sendalldm <message>
+        Example:
+          - !!sendalldm Hello all
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!sendalldm in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -229,9 +295,15 @@ class AdminCommands:
     async def senddm(self, ctx, user_id: discord.Member = None, *, MSG: str = None):
         """
         DM single user
-        First ID after ID message
+        Only the owner of this bot can use this command.
+
+        Usage:
+          -  !!senddm <member> <message>
+        Example:
+          - !!senddm PuffDip hello world
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!senddm <{user_id.id}> <{MSG}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -253,13 +325,17 @@ class AdminCommands:
             last_message = await self.bot.send_message(owner, embed=embed)
             await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
 
-
     @commands.command(pass_context=True, hidden=True)
     async def ip(self, ctx):
         """
         Show server IP
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!ip
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!ip in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -278,13 +354,17 @@ class AdminCommands:
         a = await self.bot.say(embed=embed)
         await self.bot.add_reaction(a, self.emojiUnicode['succes'])
 
-
     @commands.command(pass_context=True, hidden=True)
     async def purge(self, ctx):
         """
-        Purge bot messages
+        Purge bot messages.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!purge
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!purge in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -340,9 +420,14 @@ class AdminCommands:
     @commands.command(pass_context=True, hidden=True)
     async def git(self, ctx):
         """
-        Pull latest branch and shows information about the pull
+        Pull latest branch and shows information about the pull.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!git
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!git in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -358,13 +443,17 @@ class AdminCommands:
             a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['succes'])
 
-
     @commands.command(pass_context=True, hidden=True, name='ct')
     async def clean_terminal(self, ctx):
         """
         Cleans terminal server [LINUX]
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!ct
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!clean_terminal in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
+
         if ctx.message.author.id not in self.owner_list:
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
                                   description='Only the owner of this bot can use this command',
@@ -386,11 +475,16 @@ class AdminCommands:
             a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
 
-
     @commands.command(pass_context=True, hidden=True)
     async def mute(self, ctx, *, user_id: str = None):
         """
-        Mute user
+        Mute user. Bot roll has to be higher then user roll in order to work.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!mute <user ID>
+        Example:
+          - !!mute PuffDip
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!mute <{user_id}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
 
@@ -443,11 +537,16 @@ class AdminCommands:
             a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['error'])
 
-
     @commands.command(pass_context=True, hidden=True)
     async def unmute(self, ctx, *, user_id: str = None):
         """
-        Unmute user
+        Unmute user. Remove user who is muted from database.
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!unmute <user ID>
+        Example:
+          - !!unmute PuffDip
         """
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!mute <{user_id}> in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
 
@@ -504,8 +603,13 @@ class AdminCommands:
     @commands.command(pass_context=True)
     async def sebisauce(self, ctx):
         """
-        Sebisauce,
+        Get a image related to Sebi.
+        Sebi is a random guy with perfect code related sentences.
+
+        Usage:
+          - !!sebisauce
         """
+        print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!sebisauce in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
         await self.bot.send_typing(ctx.message.channel)
 
         async with aiohttp.ClientSession() as session:
@@ -525,6 +629,13 @@ class AdminCommands:
 
     @commands.command(pass_context=True)
     async def updatesebi(self, ctx):
+        """
+        Update sebisauce API
+        Only the owner of this bot can use this command.
+
+        Usage:
+          - !!updatesebi
+        """
 
         print(f'{datetime.date.today()} {datetime.datetime.now()} - {ctx.message.author} ran command !!sebiupdate in -- Channel: {ctx.message.channel.name} Guild: {ctx.message.server.name}')
 
@@ -583,7 +694,6 @@ class AdminCommands:
         embed.set_footer(text='http://ikbengeslaagd.com/API/sebisauce.json')
         a = await self.bot.say(embed=embed)
         await self.bot.add_reaction(a, self.emojiUnicode['succes'])
-
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
