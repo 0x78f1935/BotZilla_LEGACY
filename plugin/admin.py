dd5c7752896ea799646dabd7aabc62a6c321feeb
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-28 17:14:01 +0200

    fix typo

--- plugin/admin.py
+++ plugin/admin.py
@@ -438,7 +438,7 @@ class AdminCommands:
         else:
             pull = os.popen('git pull').read()
             embed = discord.Embed(title='{}:'.format(ctx.message.author.name),
-                                  description='Pull succes :ok_hand:\n```py\n{}\n```'.format(pull),
+                                  description='Pull success :ok_hand:\n```py\n{}\n```'.format(pull),
                                   colour=0xf20006)
             a = await self.bot.say(embed=embed)
             await self.bot.add_reaction(a, self.emojiUnicode['succes'])
