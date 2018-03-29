bc6d41df82d598e5d9450c6eb44aa14fc4099009
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 23:08:02 +0200

    add thumbnail

--- plugin/fun.py
+++ plugin/fun.py
@@ -179,13 +179,13 @@ class Images:
                 if len(all_emoji) >= limit:
                     break
 
-        print(all_emoji)
-
         if all_emoji:
+            thumbnail_random = random.choice(all_emoji)
             emoji = ' -- '.join(all_emoji)
             embed = discord.Embed(title="{}".format(ctx.message.author.name),
                                   description=emoji,
                                   color=0xf20006)
+            embed.set_image(url=thumbnail_random.url)
             last_message = await self.bot.say(embed=embed)
             await self.bot.add_reaction(last_message, self.emojiUnicode['succes'])
         else:
