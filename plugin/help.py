8cd4b8cc060bc5cedb79f345d8655152cea19af7
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 16:41:10 +0200

    test

--- plugin/help.py
+++ plugin/help.py
@@ -212,8 +212,7 @@ class Help:
                     number__input = await self.bot.say(embed=embed)
                     msg = await self.bot.wait_for_message(author=ctx.message.author, timeout=120)
                     if msg.content is int(msg.content):
-                        old_page_number = page_number
-                        if old_page_number >= 0 and old_page_number <= lenght_help:
+                        if int(msg.content) >= 0 and int(msg.content) <= lenght_help:
                             page_number = int(msg.content)
                     else:
                         embed = discord.Embed(title=f'Help for: {ctx.message.author.display_name}',
