3311a0ce458d96e105993b45de92a84d89264689
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 14:51:40 +0200

    hotfix

--- plugin/help.py
+++ plugin/help.py
@@ -201,12 +201,13 @@ class Help:
                         page_number = lenght_help
                         # print(page_number)
 
+                # to do - ignore page 0
                 embed = paginator[str(page_number)]
-                content_embed = start.embeds[0]['description'.split('\n')[0].replace(f'-- {self.config["prefix"]}', '')]
-                self.database.cur.execute(f"select * from botzilla.help where name = {content_embed}")
-                catagory = self.database.cur.fetchone()
+                # content_embed = start.embeds[0]['description'.split('\n')[0].replace(f'-- {self.config["prefix"]}', '')]
+                # self.database.cur.execute(f"select * from botzilla.help where name = {content_embed}")
+                # catagory = self.database.cur.fetchone()
 
-                embed.set_footer(text=f'| Category: {catagory[1]} | Version: {self.version}\t|\tDev help: !!rtfm\t|\tPage: {int(page_number + 1)}/{int(len(paginator.keys()))} |')
+                embed.set_footer(text=f'| Category: - | Version: {self.version}\t|\tDev help: !!rtfm\t|\tPage: {int(page_number + 1)}/{int(len(paginator.keys()))} |')
                 await self.bot.edit_message(start, embed=embed)
 
         # if command give info about that command
