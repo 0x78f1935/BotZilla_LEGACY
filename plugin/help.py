a6387952e57a328a2a15913d1fee68ee879cd9ac
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 15:45:47 +0200

    test

--- plugin/help.py
+++ plugin/help.py
@@ -208,7 +208,7 @@ class Help:
                 try:
                     content_embed = new.embeds[0]['fields'][0]['name']
                     print(content_embed)
-                    content_embed = content_embed.split('\n')[0]
+                    content_embed = content_embed.split('\n')[0].replace('-- !!', '')
                     print(content_embed)
                     self.database.cur.execute(f"select * from botzilla.help where name = {content_embed}")
                     catagory = self.database.cur.fetchone()
