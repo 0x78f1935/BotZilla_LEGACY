6ebeba0509dd986a757a83f5062e99830f28e700
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 14:23:29 +0200

    fix bug

--- plugin/help.py
+++ plugin/help.py
@@ -109,7 +109,7 @@ class Help:
                                 inline=False)
                 pages.append(new_page)
             # print('DONE New_page Function')
-            return (pages, cog)
+            return pages
 
         def generate_pages():
             # print('generate_pages Function')
@@ -173,7 +173,7 @@ class Help:
             # remove duplicates
             page = 0
             paginator = {}
-            paginator['0'] = (page0, 'Welkom')
+            paginator['0'] = page0
             for key, value in generate_pages_result.items():
                 paginator[key] = value[0]
                 page += 1
@@ -216,7 +216,7 @@ class Help:
                         # print(page_number)
 
                 embed = paginator[str(page_number)]
-                embed[0].set_footer(text=f'| Category: {embed[1]} | Version: {self.version}\t|\tDev help: !!rtfm\t|\tPage: {int(page_number + 1)}/{int(len(paginator.keys()))} |')
+                embed.set_footer(text=f'| Category: {footer_cog} | Version: {self.version}\t|\tDev help: !!rtfm\t|\tPage: {int(page_number + 1)}/{int(len(paginator.keys()))} |')
                 await self.bot.edit_message(start, embed=embed)
 
         # if command give info about that command
