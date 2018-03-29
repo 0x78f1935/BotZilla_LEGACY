41fb93bbbe2a268dc58a05455fa4d0bf504060ad
Annihilator708 <Annihilator708@users.noreply.github.com>
2018-03-29 21:20:29 +0200

    test

--- plugin/fun.py
+++ plugin/fun.py
@@ -166,7 +166,7 @@ class Images:
         print(all_emoji)
 
         if all_emoji:
-            emoji = ' -- '.join([x.name for x in all_emoji])
+            emoji = ' -- '.join([x for x in all_emoji])
             print(emoji)
             embed = discord.Embed(title="{}".format(ctx.message.author.name),
                                   description=emoji,
